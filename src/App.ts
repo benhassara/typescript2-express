d5c7508d987f48cd0427d7b24dd7199600bcd089
benhassara <bhassara@gmail.com>
2016-10-28 09:22:12 -0600

    one hero route

--- src/App.ts
+++ src/App.ts
@@ -33,17 +33,7 @@ class App {
    * Configure API endpoints.
    */
   private routes(): void {
-    /* This is just to get up and running, and to make sure what we've got is
-     * working so far. This function will change when we start to add more
-     * API endpoints */
-    // let router = express.Router();
-    // // placeholder route handler
-    // router.get('/', (req, res, next) => {
-    //   res.json({
-    //     message: 'Hello World!'
-    //   });
-    // });
-    this.express.use('/api/v1', HeroRouter);
+    this.express.use('/api/v1/heroes', HeroRouter);
   }
 }
 
